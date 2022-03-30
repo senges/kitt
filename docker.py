2be9f6bdf12d8997a6e34d90ccafc38c21c1d01a
senges <charles.senges.io+github@protonmail.com>
2022-03-30 13:49:43 +0200

    core(docker.py): add docker wrapper

---
+++ docker.py
@@ -0,0 +1,22 @@
+import docker
+import dockerpty
+
+# Docker wrapper
+class Docker:
+    def __init__():
+        pass
+
+    def run(self):
+        pass
+
+    def pull(self):
+        pass
+
+    def stat(self):
+        pass
+
+    def volumes(self):
+        pass
+
+    def env(self):
+        pass
