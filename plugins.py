c369a63f41a63c48e11f259e6c8a0ed3f656a01b
senges <charles.senges.io+github@protonmail.com>
2022-05-05 15:08:49 +0200

    feat(plugins): add multiple plugins, better config files

--- plugins.py
+++ plugins.py
@@ -14,13 +14,26 @@ class KittPlugin(AbstractClass):
         cmdset = [ '# Plugin:' + self.name ]
         cmdset += self._generate()
 
-        return '\n'.join(cmdset) + '\n'
+        return cmdset
 
     # Should be implemented for each inherited Class of KittPlugin
     # Must return a list of lines in dockerfile format.
     @abstractmethod
     def _generate(self) -> [str]:
         pass
+
+class BashPlugin(KittPlugin):
+    def _generate(self) -> str:
+        cmd = 'RUN echo "# Kitt Customs" >> /root/.bashrc'
+
+        for extra in self.config.get('extras', []):
+            cmd += ' && echo "%s" >> /root/.bashrc' % extra
+        
+        for alias in self.config.get('alias', []):
+            name, cmd = alias['name'], alias['cmd']
+            cmd += ' && echo alias %s="%s" >> /root/.bashrc' % (name, cmd)
+
+        return [ cmd ]
 
 class ZshPlugin(KittPlugin):
     def _generate(self) -> str:
@@ -42,13 +55,13 @@ class ZshPlugin(KittPlugin):
             'cmd': 'catalog --list'
         })
 
-        for plugin in self.config['plugins']:
+        for plugin in self.config.get('plugins', []):
             cmd += ' -p "%s"' % plugin
 
-        for extra in self.config['extras']:
+        for extra in self.config.get('extras', []):
             cmd += ' -a "%s"' % extra
 
-        for alias in self.config['alias']:
+        for alias in self.config.get('alias', []):
             cmd += ' -a \'alias %s="%s"\'' % (alias['name'], alias['cmd'])
 
         cmdset.append(cmd)
@@ -60,7 +73,7 @@ class TmuxPlugin(KittPlugin):
     def _generate(self) -> str:
         cmd = 'RUN catalog -v tmux'
 
-        for extra in self.config['extras']:
+        for extra in self.config.get('config', []):
             cmd += ' && echo "%s" >> /root/.tmux.conf' % extra
 
         return [ cmd ]
@@ -69,15 +82,51 @@ class ScreenPlugin(KittPlugin):
     def _generate(self) -> str:
         cmd = 'RUN catalog -v screen'
 
-        for extra in self.config['extras']:
+        for extra in self.config.get('config', []):
             cmd += ' && echo "%s" >> /root/.screenrc' % extra
 
         return [ cmd ]
 
+class CopyPlugin(KittPlugin):
+    def _generate(self) -> str:
+        cmdset = []
+
+        for file in self.config.get('files', []):
+            src, dest = file['src'], file['dest']
+            cmdset.append('COPY %s %s' % (src, dest))
+
+        return cmdset
+
+class DownloadPlugin(KittPlugin):
+    def _generate(self) -> str:
+        cmdset = []
+
+        for res in self.config.get('ressources', []):
+            url, target = res['url'], res['target']
+            cmd = 'RUN wget -O %s %s --no-check-certificate' %s (target, url)
+            cmdset.append(cmd)
+
+        return cmdset
+
+class GitPlugin(KittPlugin):
+    def _generate(self) -> str:
+        cmdset = []
+
+        for repo in self.config.get('repos', []):
+            url, target = repo['url'], repo['target']
+            cmd = 'RUN git clone %s %s' %s (url, target)
+            cmdset.append(cmd)
+
+        return cmdset
+
 plugins = {
+    'bash'   : BashPlugin,
     'zsh'    : ZshPlugin,
     'tmux'   : TmuxPlugin,
-    'screen' : ScreenPlugin
+    'screen' : ScreenPlugin,
+    'copy'   : CopyPlugin,
+    'git'    : GitPlugin,
+    'download' : DownloadPlugin,
 }
 
 def compose(name: str, conf: dict) -> str:
